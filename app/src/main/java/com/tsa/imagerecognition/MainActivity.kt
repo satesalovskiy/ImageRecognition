bd59579f9bf9cc69de36a3fdea64d2b2de71d7de
Sergey Tesalovskiy <tesser.99.2012@gmail.com>
2020-04-17 15:56:07 +0300

    Version 1.0. MainActivity and ARFragment now on Kotlin

---
+++ app/src/main/java/com/tsa/imagerecognition/MainActivity.kt
@@ -0,0 +1,21 @@
+
+package com.tsa.imagerecognition
+
+import android.os.Bundle
+import android.widget.Toast
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.findFragmentById(R.id.ux_fragment)
+        auth = FirebaseAuth.getInstance()
+        Toast.makeText(this, auth.uid, Toast.LENGTH_LONG).show()
+    }
+}
